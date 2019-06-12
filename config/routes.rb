b479312b2ddcc4a3c6416e0533e2c97629c079df
Bluesbaka <badunnells@gmail.com>
2019-06-11 17:20:03 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:index, :edit, :new, :update, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
